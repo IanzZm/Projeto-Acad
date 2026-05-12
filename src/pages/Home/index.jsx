b301dd5556b96ecba098f8e2e6356147bbe06ddb
Lorena Celani <lorenacelanii@gmail.com>
2026-05-12 10:46:42 -0300

    feat: tela inicial

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -22,6 +22,8 @@ export function Home() {
               <button className={styles.primaryButton}>Inscreva-se</button>
               <button className={styles.secondaryButton}>Saiba mais</button>
             </div>
+          </div>
+
             <div className={styles.heroImageArea}>
               <img
                 src={fotoPersonal}
@@ -29,7 +31,7 @@ export function Home() {
                 className={styles.heroImage}
               />
             </div>             
-            </div>
+            
         </section>
       </main>
     </>
