99634a9a8d669f1c962b1904f2ee0e7b3adcfff2
Lorena Celani <lorenacelanii@gmail.com>
2026-05-15 11:47:35 -0300

    tela de avaliações

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import { HeroSection } from "../../components/HeroSection";
 import { ServicesSection } from "../../components/ServicesSection";
 import { AboutSection } from "../../components/AboutSection";
 import { ContactSection } from "../../components/ContactSection";
+import { RateSection } from "../../components/RateSection";
 
 
 export function Home() {
@@ -14,6 +15,7 @@ export function Home() {
         <HeroSection />
         <ServicesSection />
         <AboutSection />
+        <RateSection/>
         <ContactSection/>
       </main>
       <Footer />
