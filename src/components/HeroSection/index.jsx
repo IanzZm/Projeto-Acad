a713b16c34dfed5b4e1afd896c690f42a57e541f
ianucasm <ianlucas2014t@gmail.com>
2026-05-14 22:04:27 -0300

    feat: botoes da herosection funcionais

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,32 +1,40 @@
 import styles from "./styles.module.css";
-import fotoPersonal from "../../assets/indio.png";  
+import fotoPersonal from "../../assets/indio.png";
+import { Link } from "react-router-dom";
 export function HeroSection() {
-    return (
-        <main className={styles.home}>
-        <section className={styles.hero} id="topo">
-          <div className={styles.content}>
-            <h1 className={styles.title}>
-              Transforme seu corpo com <span>treino</span> e <span>estratégia</span>.
-            </h1>
+  return (
+    <main className={styles.home}>
+      <section className={styles.hero} id="topo">
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            Transforme seu corpo com <span>treino</span> e{" "}
+            <span>estratégia</span>.
+          </h1>
 
-            <p className={styles.description}>
-              Treinamento personalizado para hipertrofia, emagrecimento e performance.
-            </p>
+          <p className={styles.description}>
+            Treinamento personalizado para hipertrofia, emagrecimento e
+            performance.
+          </p>
 
-            <div className={styles.buttons}>
-              <button className={styles.primaryButton}>Inscreva-se</button>
-              <button className={styles.secondaryButton}>Saiba mais</button>
-            </div>
+          <div className={styles.buttons}>
+            
+            <Link to="/login" className={styles.primaryButton}>
+              Inscreva-se
+            </Link>
+            <a href="#sobre" className={styles.secondaryButton}>
+              Saiba Mais
+            </a>
           </div>
+        </div>
 
-            <div className={styles.heroImageArea}>
-              <img
-                src={fotoPersonal}
-                alt="Personal trainer"
-                className={styles.heroImage}
-              />
-            </div>             
-        </section>
-        </main>
-    );
+        <div className={styles.heroImageArea}>
+          <img
+            src={fotoPersonal}
+            alt="Personal trainer"
+            className={styles.heroImage}
+          />
+        </div>
+      </section>
+    </main>
+  );
 }
