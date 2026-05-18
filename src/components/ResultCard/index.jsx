0b71b78d2568cfac0a5d34b0f16ea5c2f74826cb
Lorena Celani <lorenacelanii@gmail.com>
2026-05-18 19:08:49 -0300

    feat: adição dos cards e fix no css

--- src/components/ResultCard/index.jsx
+++ src/components/ResultCard/index.jsx
@@ -4,8 +4,10 @@ export function ResultCard({ image, text, name }) {
   return (
     <article className={styles.card}>
       <img src={image} alt={name} className={styles.cardImage} />
-      <p className={styles.cardText}>{text}</p>
-      <h3 className={styles.cardName}>{name}</h3>
+      <div className={styles.cardContent}>
+        <p className={styles.cardText}>{text}</p>
+        <h3 className={styles.cardName}>{name}</h3>
+      </div>
     </article>
   );
 }
